104954bae44bc77735b4065251e62ef67f5a419f
VGunjan <CapsLock@9379>
2019-11-03 19:05:08 +0530

    Kafka

--- src/main/java/com/gunjan/tweet/kafka/producer/WriteTweetToKafka.java
+++ src/main/java/com/gunjan/tweet/kafka/producer/WriteTweetToKafka.java
@@ -1,12 +1,15 @@
 package com.gunjan.tweet.kafka.producer;
 
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import twitter4j.StallWarning;
@@ -25,25 +28,39 @@ public class WriteTweetToKafka
         ConfigurationBuilder cb = new ConfigurationBuilder();
         
         cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("")
-                .setOAuthConsumerSecret("")
-                .setOAuthAccessToken("")
-                .setOAuthAccessTokenSecret("");
+                .setOAuthConsumerKey("yDnPCKNBtTF5h5FrB9KtkDbbn")
+                .setOAuthConsumerSecret("kvDVDsh9N4Pl549zPcX7mToHG7Qyq3FjXTDpj7JdoxjMKqafsp")
+                .setOAuthAccessToken("448920351-fPR5XURoRdu5fYjzSfGk56kPZN1NWWvBpzczAqmf")
+                .setOAuthAccessTokenSecret("Guz9Oq1qC2cgXyOJo85zpZmO7n5J6oD4q6p88fdtXymFL");
         
         TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
         
         Producer<String,String> producer = new KafkaProducer<>(getKafkaProperties());
         twitterStream.addListener(new StatusListener()
         {
+            AtomicInteger tweetCount = new AtomicInteger(0);
+            
             @Override
             public void onStatus(Status status)
             {
                 try
                 {
-                    System.out.println(status);
-                    producer.send(new ProducerRecord<>("admintome-test", new ObjectMapper().writeValueAsString(status)));
+                    
+                    //System.out.println(tweetCount.getAndIncrement() + "=> " + status.getText());
+                    //root@k8s-master:/home/ubuntu/TweetCount/kafka_2.11-2.3.0/bin# ./kafka-topics.sh --create --bootstrap-server 10.71.69.236:31117 --replication-factor 3 --partitions 6
+                    // --config min.insync.replicas=3 --topic tweeter-topic
+                    Callback callback = new Callback()
+                    {
+                        @Override
+                        public void onCompletion(RecordMetadata metadata, Exception exception)
+                        {
+                            System.out.println(metadata);
+                            exception.printStackTrace();
+                        }
+                    };
+                    producer.send(new ProducerRecord("tweeter-topic", new ObjectMapper().writeValueAsString(status)), callback);
                 }
-                catch(JsonProcessingException e)
+                catch(Exception e)
                 {
                     e.printStackTrace();
                 }
@@ -57,7 +74,7 @@ public class WriteTweetToKafka
             @Override
             public void onTrackLimitationNotice(int numberOfLimitedStatuses)
             {
-                System.out.println("onTrackLimitationNotice = " +  numberOfLimitedStatuses);
+                System.out.println("onTrackLimitationNotice = " + numberOfLimitedStatuses);
             }
             
             @Override
@@ -78,7 +95,7 @@ public class WriteTweetToKafka
         });
         
         twitterStream.sample();
-        System.out.println("Message sent successfully Gunjan");
+        System.out.println("Message sent successfully");
     }
     
     private static Properties getKafkaProperties()
@@ -86,28 +103,34 @@ public class WriteTweetToKafka
         Properties props = new Properties();
         
         //Assign localhost id
-        props.put("bootstrap.servers", "10.71.69.236:31440");
+        props.put("bootstrap.servers", "10.71.69.236:31117,10.71.69.236:31118,10.71.69.236:31119");
         
         //Set acknowledgements for producer requests.
-        props.put("acks", "all");
+        props.put("acks", "-1");
+        
+        props.put("enable.idempotence", true);
         
         //If the request fails, the producer can automatically retry,
-        props.put("retries", 0);
+        props.put("retries", 10);
         
         //Specify buffer size in config
-        props.put("batch.size", 16384);
+        props.put("batch.size", 32768);
         
         //Reduce the no of requests less than 0
-        props.put("linger.ms", 1);
+        props.put("linger.ms", 20);
         
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        props.put("buffer.memory", 33554432);
+        props.put("compression.type", "snappy");
         
-        props.put("key.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
+        //If the producer produces faster than the broker can take, the records will be buffered in memory.
+        // Below is the size of the send buffer.
+        props.put("buffer.memory", 33554432); // 32 MB
         
-        props.put("value.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
+        // If send buffer is full, producer will block for 60000 ms before throwing exception
+        props.put("max.block.ms", 60000);
+        
+        props.put("key.serializer", StringSerializer.class.getName());
+        
+        props.put("value.serializer", StringSerializer.class.getName());
         return props;
     }
 }
